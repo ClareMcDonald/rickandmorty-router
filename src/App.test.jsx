63707a9924cbd4e92cef6e1c6e844d286bae6571
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-02 16:25:29 -0700

    wrote app test, test fails

---
+++ src/App.test.jsx
@@ -0,0 +1,22 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+
+describe(<App />, () => {
+    it('renders a list of characters,' async () => {
+        render(
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        );
+
+        screen.getByText('/loading/i');
+
+        const link = await screen.findByText('Morty Smith');
+        
+        userEvent.click(link);
+
+        await screen.findByAltText('Image of Morty Smith');
+    });
+});
