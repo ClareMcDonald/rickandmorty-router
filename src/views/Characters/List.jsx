f3d585a4c9e535b2072282b712afbfd55b3bb686
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-02 16:57:06 -0700

    set state in List

--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function List() {
+    const history = useHistory();
+    const location = useLocation();
+    const status = new URLSearchParams(location.search).get('status') ?? 'all';
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+    }, []);
+
   return (
     <div>List</div>
   )
