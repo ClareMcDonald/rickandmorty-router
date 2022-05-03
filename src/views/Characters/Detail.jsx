0a006fa95f0c0a559b1d403d93c2ecb035d83988
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-03 11:52:57 -0700

    wrote useEffect for detail page

--- src/views/Characters/Detail.jsx
+++ src/views/Characters/Detail.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
 
 export default function Detail() {
+  const [character, setCharacter] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchCharacter() {
+      const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+
+      const characterInfo = await res.json();
+      setCharacter(characterInfo);
+      setLoading(false);
+    };
+    fetchCharacter();
+
+  }, []);
+
   return (
     <div>Detail</div>
   )
